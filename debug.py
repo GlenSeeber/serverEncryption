bd150e12ef361503f5e99fbf5d84e158c55378da
Glen Seeber <gseeber5@gmail.com>
2022-06-10 19:32:38 -0700

    put RSA into easy funcs it's easier to impliment it into the project if we use funcs, I can just copy paste them in now.
    First generate a key, next save those values where you will. Send any keys you need to. Use
    encryptMsg() to encrypt it with your key. Then use decryptMsg() to make it readable again.
    You can send your keys as strings and it will still work.

--- debug.py
+++ debug.py
@@ -2,6 +2,28 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 
+FORMAT = 'utf-8'
+
+
+def encryptMsg(msg, pubKey):
+    pubKey = RSA.importKey(pubKey)
+
+    # create a cipher object for the key
+    cipher = PKCS1_OAEP.new(pubKey)
+    
+    #encrypt            (convert msg to bytes)
+    output = cipher.encrypt(msg.encode())
+    return output
+
+def decryptMsg(msg, privKey):
+    privKey = RSA.importKey(privKey)
+
+    cipher = PKCS1_OAEP.new(privKey)
+
+    output = cipher.decrypt(msg).decode(FORMAT)
+    return output
+
+# run
 message = 'You can attack now!'
 
 # get a key using random bytes
@@ -9,6 +31,16 @@ key = RSA.generate(2048, get_random_bytes)
 # key means private key, pubKey means public key
 # the public key is deriven from the private key, but not the other way around
 
+# set keys from key
+pubKey = key.publickey().export_key("OpenSSH")
+privKey = key.export_key('PEM')
+
+encryptedMsg = encryptMsg(message, pubKey)
+decryptedMsg = decryptMsg(encryptedMsg, privKey)
+
+print(f"Original:\n{message}\n\nEncrypted:\n{encryptedMsg}\n\nDecrypted:\n{decryptedMsg}")
+
+""" 
 #write Private Key to a pem file
 with open('myKey.pem', 'wb') as f:
     f.write(key.export_key('PEM'))
@@ -22,10 +54,6 @@ with open('pubKey.pem', 'wb') as f:
 
 #get our public key (it's just gonna be a memory reference)
 pubKey = RSA.importKey(open('pubKey.pem').read())
-
-#convert the memory reference into the actual data stored there
-#pubKey = pubKey.exportKey(format='PEM', passphrase=None, pkcs=1)
-
 #okay now let's encrypt a message using pubKey
 
 #create a cipher object for our key
@@ -42,4 +70,4 @@ decryptedMsg = cipher.decrypt(encryptedMsg).decode('utf-8')
 
 print(f"back to the original message:\n{decryptedMsg}")
 
-print(f"is it the same? - {message == decryptedMsg}")
+print(f"is it the same? - {message == decryptedMsg}") """
