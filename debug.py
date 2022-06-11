214953003ecd28148bdc1b2403b31539ea8707b3
Glen Seeber <gseeber5@gmail.com>
2022-06-10 19:17:25 -0700

    make working RSA encryption prototype I removed the old code on debug.py (we don't need it), and created a new script on it that uses the the Crypto library (well, pycryptodome, pycrypto is depricated) to do RSA encryption on a given string message.
    It is able to convert keys from strings into the actual keys, both private and public, without
    needing access to the other. This should be fairly easy to impliment into our server-client
    product now.

--- debug.py
+++ debug.py
@@ -1,28 +1,45 @@
-import rsa
-from cryptography.fernet import Fernet
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Random import get_random_bytes
 
-def convert(string, breaker):
-    li = list(string.split(breaker))
-    return li
+message = 'You can attack now!'
 
-# msg contains the public key
-msg = "!SEND_KEY::PublicKey(8048476234717738932734989159515624270210214717354379368403794219887970986450405471004819145347376598718294789720508485696447068834108248875002388186192013, 65537)"
+# get a key using random bytes
+key = RSA.generate(2048, get_random_bytes)
+# key means private key, pubKey means public key
+# the public key is deriven from the private key, but not the other way around
 
-#isolate
-pubKey = convert(msg, '::')[1]
-# encode into bytes
-pubKey = pubKey.encode()
+#write Private Key to a pem file
+with open('myKey.pem', 'wb') as f:
+    f.write(key.export_key('PEM'))
 
-#print(f"public key recieved:\n{pubKey}\n")
-"""
-key = Fernet.generate_key()
-fernet = Fernet(key)
-"""
+#write the Public Key to a txt file
+pubKey = key.publickey().export_key("OpenSSH")
+with open('pubKey.pem', 'wb') as f:
+    f.write(pubKey)
 
-key = 'hello geeks'.encode()
+# ----this code could be run on a seperate script theoretically----
 
-# encrypt the symmetric key using the public asymmetric key that
-# the client sent us. (this codeline is using !!rsa NOT fernet!!)
-print(f"\n\nMessage: {key}\n")
-print(f"\n\npublicKey: {pubKey}\n")
-output = rsa.encrypt(key, pubKey)
+#get our public key (it's just gonna be a memory reference)
+pubKey = RSA.importKey(open('pubKey.pem').read())
+
+#convert the memory reference into the actual data stored there
+#pubKey = pubKey.exportKey(format='PEM', passphrase=None, pkcs=1)
+
+#okay now let's encrypt a message using pubKey
+
+#create a cipher object for our key
+cipher = PKCS1_OAEP.new(pubKey)
+
+encryptedMsg = cipher.encrypt(message.encode())
+
+print(f"\n    jumbled message:\n{encryptedMsg}\n\n")
+
+#okay now decrypt
+cipher = PKCS1_OAEP.new(key)
+
+decryptedMsg = cipher.decrypt(encryptedMsg).decode('utf-8')
+
+print(f"back to the original message:\n{decryptedMsg}")
+
+print(f"is it the same? - {message == decryptedMsg}")
